f9ba777db75b8ea76de1846c46eb31a6b4bf6a49
i know <ik39.10vevic@gmail.com>
2025-05-20 14:25:53 +0900

    uproom

---
+++ craco.config.js
@@ -0,0 +1,22 @@
+const webpack = require("webpack");
+
+module.exports = {
+  webpack: {
+    configure: (config) => {
+      config.resolve.fallback = {
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
+        buffer: require.resolve("buffer"),
+      };
+
+      config.plugins = [
+        ...(config.plugins || []),
+        new webpack.ProvidePlugin({
+          Buffer: ["buffer", "Buffer"],
+        }),
+      ];
+
+      return config;
+    },
+  },
+};
