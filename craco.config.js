3e48cf4703548461a53acb78f8a9b1014856ac4a
i know <ik39.10vevic@gmail.com>
2025-05-20 14:33:10 +0900

    Enable signed CloudFront playback & build polyfills

--- craco.config.js
+++ craco.config.js
@@ -2,21 +2,22 @@ const webpack = require("webpack");
 
 module.exports = {
   webpack: {
-    configure: (config) => {
-      config.resolve.fallback = {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        ...(webpackConfig.resolve.fallback || {}),
         crypto: require.resolve("crypto-browserify"),
         stream: require.resolve("stream-browserify"),
-        buffer: require.resolve("buffer"),
+        buffer: require.resolve("buffer/"),
       };
 
-      config.plugins = [
-        ...(config.plugins || []),
+      webpackConfig.plugins = [
+        ...(webpackConfig.plugins || []),
         new webpack.ProvidePlugin({
           Buffer: ["buffer", "Buffer"],
         }),
       ];
 
-      return config;
+      return webpackConfig;
     },
   },
 };
