9acc4d632d0eb3044f5ac0497debe051bb6ade12
i know <ik39.10vevic@gmail.com>
2025-05-20 14:57:22 +0900

    Build succeeded: deploy ready

--- craco.config.js
+++ craco.config.js
@@ -4,16 +4,19 @@ module.exports = {
   webpack: {
     configure: (webpackConfig) => {
       webpackConfig.resolve.fallback = {
-        ...(webpackConfig.resolve.fallback || {}),
+        ...webpackConfig.resolve.fallback,
         crypto: require.resolve("crypto-browserify"),
         stream: require.resolve("stream-browserify"),
         buffer: require.resolve("buffer/"),
+        vm: require.resolve("vm-browserify"),
+        process: require.resolve("process/browser.js"),
       };
 
       webpackConfig.plugins = [
         ...(webpackConfig.plugins || []),
         new webpack.ProvidePlugin({
           Buffer: ["buffer", "Buffer"],
+          process: "process/browser.js",
         }),
       ];
 
